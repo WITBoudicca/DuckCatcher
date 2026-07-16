7ca13dca03f164c7ec92e0606fbc4b6411e45a7f
NikkiLF <nikkilovefabrizio@gmail.com>
2026-07-15 21:11:16 -0400

    fixed return duck dialogue

--- system/ending/ending.gd
+++ system/ending/ending.gd
@@ -2,10 +2,11 @@ extends Node3D
 
 @onready var credits = $CanvasLayer/RichTextLabel
 
+
 func _ready() -> void:
 	credits.modulate.a = 0.0
 	await get_tree().create_timer(2).timeout
 	var tween = get_tree().create_tween()
-	tween.tween_property(credits, "modulate:a", 2.0, 1.0)
+	tween.tween_property(credits, "modulate:a", 2.0, 2.0)
 	await get_tree().create_timer(5).timeout
 	SceneTransition.load_scene("res://system/ui/start menu/start_menu.tscn")
